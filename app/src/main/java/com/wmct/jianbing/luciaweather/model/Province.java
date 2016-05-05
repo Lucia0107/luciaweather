3eef449242729f230be2bef4e00ab803f949d347
Lucia <chuanhuijian@163.com>
2016-05-05 15:49:35 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/wmct/jianbing/luciaweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.wmct.jianbing.luciaweather.model;
+
+/**
+ * Created by jianbing on 2016/5/5.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+}
